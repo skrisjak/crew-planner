8a5c40f85c782fa0803b69e847c44dda8b11b5e4
Jakub Skříšovský <kuba-skrisovsky@seznam.cz>
2025-11-07 23:35:29 +0100

    Structure remake, added item management

--- frontend/src/api/CONF.js
+++ frontend/src/api/CONF.js
@@ -1,6 +1,6 @@
 const CONF  = {
-    //origin: process.env.REACT_APP_URL? process.env.REACT_APP_URL : window.location.origin + '/',
-    origin: "http://localhost:8080/",
+    origin: process.env.REACT_APP_URL? process.env.REACT_APP_URL : window.location.origin + '/',
+    //origin: "http://localhost:8080/",
 
     longitude: 14.4678919,
     latitude: 48.9711547,
